09ace36cd6a47f2fa43e0c7280858d978a04a697
Ivan Puzyrev <i.puzyrev@arammeem.com>
2023-02-09 18:05:11 +0200

    Post navigator commands on execute.

---
+++ library/stub-android/src/main/java/android/os/Handler.java
@@ -0,0 +1,14 @@
+/*
+ * ToYou
+ * Copyright © 2023 Aram Meem Company Limited. All Rights Reserved.
+ */
+
+package android.os;
+
+public class Handler {
+    public Handler(Looper looper) {}
+
+    public boolean post(Runnable runnable){
+        return false;
+    }
+}
